b0ecb7b00a216a09924de174d719b4c4723ba6dd
Nacho <nachocmstrato@gmail.com>
2019-02-09 01:31:23 +0100

    RGBD camera working on ROS.

--- followperson.py
+++ followperson.py
@@ -12,17 +12,20 @@ import signal
 
 # from PyQt5 import QtWidgets
 
-from Camera.camera import Camera
-from Camera.threadcamera import ThreadCamera
+from Camera.ROSCam import ROSCam
+# from Camera.threadcamera import ThreadCamera
 from GUI.threadgui import ThreadGUI
 from Net.threadnetwork import ThreadNetwork
 from Motors.threadmotors import ThreadMotors
+import rospy
 
 
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 if __name__ == '__main__':
+
+    rospy.init_node("followperson")
     topics = {'rgb': '/camera/rgb/image_raw',
               'depth': '/camera/depth_registered/image_raw'}
 
@@ -64,63 +67,67 @@ if __name__ == '__main__':
     # else:
     #     raise SystemExit(('%s not supported! Supported devices: Kobuki, PTZ') % (device_type))
 
+    import cv2
+
+    cam = ROSCam(topics)
+    while not rospy.is_shutdown():
+        cv2.imshow("RGB", cam.rgb_img)
+        cv2.imshow("depth", cam.depth_img)
+        if cv2.waitKey(1) == 27:
+            break
+    cv2.destroyAllWindows()
+
+    # network = TrackingNetwork(net_prop)
+    # network.setCamera(cam)
+    # t_network = ThreadNetwork(network)
+    # t_network.start()
 
 
-    cam = Camera(topics)
-    t_cam = ThreadCamera(cam)
-    t_cam.start()
 
-    network = TrackingNetwork(net_prop)
-    network.setCamera(cam)
-    t_network = ThreadNetwork(network)
-    t_network.start()
+    # mom_path = cfg.getProperty('FollowPerson.Mom.ImagePath')
+
+    # siamese_network = SiameseNetwork(siamese_model, mom_path)
 
 
+    # app = QtWidgets.QApplication(sys.argv)
+    # window = GUIClass()
 
-    mom_path = cfg.getProperty('FollowPerson.Mom.ImagePath')
-
-    siamese_network = SiameseNetwork(siamese_model, mom_path)
-
-
-    app = QtWidgets.QApplication(sys.argv)
-    window = GUIClass()
-
-    if device_type.lower() == 'kobuki':
-        depth_proxy = jdrc.getCameraClient('FollowPerson.Depth')
-        depth = Camera(depth_proxy)
-        network.setDepth(depth)
-        t_depth = ThreadCamera(depth)
-        t_depth.start()
-        window.setCamera(cam, t_cam)
-        window.setDepth(depth, t_depth)
-    else:
-        window.setCamera(cam, t_cam)
+    # if device_type.lower() == 'kobuki':
+    #     depth_proxy = jdrc.getCameraClient('FollowPerson.Depth')
+    #     depth = Camera(depth_proxy)
+    #     network.setDepth(depth)
+    #     t_depth = ThreadCamera(depth)
+    #     t_depth.start()
+    #     window.setCamera(cam, t_cam)
+    #     window.setDepth(depth, t_depth)
+    # else:
+    #     window.setCamera(cam, t_cam)
 
 
-    motors = Motors(motors_proxy)
-    motors.setNetworks(network, siamese_network)
-    motors.setCamera(cam)
-    if device_type.lower() == 'kobuki':
-        motors.setDepth(depth)
-    t_motors = ThreadMotors(motors)
-    window.setMotors(motors, t_motors)
-    t_motors.start()
+    # motors = Motors(motors_proxy)
+    # motors.setNetworks(network, siamese_network)
+    # motors.setCamera(cam)
+    # if device_type.lower() == 'kobuki':
+    #     motors.setDepth(depth)
+    # t_motors = ThreadMotors(motors)
+    # window.setMotors(motors, t_motors)
+    # t_motors.start()
 
 
-    window.setNetwork(network, t_network)
-    window.show()
+    # window.setNetwork(network, t_network)
+    # window.show()
 
-    # Threading GUI
-    t_gui = ThreadGUI(window)
-    t_gui.start()
+    # # Threading GUI
+    # t_gui = ThreadGUI(window)
+    # t_gui.start()
 
 
-    print("")
-    print("Requested timers:")
-    print("    Camera: %d ms" % (t_cam.t_cycle))
-    print("    GUI: %d ms" % (t_gui.t_cycle))
-    print("    Network: %d ms" % (t_network.t_cycle))
-    print("    Motors: %d ms" % (t_motors.t_cycle))
-    print("")
+    # print("")
+    # print("Requested timers:")
+    # print("    Camera: %d ms" % (t_cam.t_cycle))
+    # print("    GUI: %d ms" % (t_gui.t_cycle))
+    # print("    Network: %d ms" % (t_network.t_cycle))
+    # print("    Motors: %d ms" % (t_motors.t_cycle))
+    # print("")
 
-    sys.exit(app.exec_())
+    # sys.exit(app.exec_())
